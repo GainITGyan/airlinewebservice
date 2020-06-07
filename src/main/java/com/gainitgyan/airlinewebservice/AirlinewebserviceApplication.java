4f23f517d384722f303196df1dd8b921e1269c9f
OPTIPLEX <OPTIPLEX@host.docker.internal>
2020-06-07 14:25:11 -0700

    Initial commit

---
+++ src/main/java/com/gainitgyan/airlinewebservice/AirlinewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gainitgyan.airlinewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinewebserviceApplication.class, args);
+	}
+
+}
